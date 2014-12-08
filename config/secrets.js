dcc1167e0b5478f1897763b26e26d33a249a5f97
Junjue <wang.junju@husky.neu.edu>
2014-12-08 02:58:44 -0500

    Updated oAuth

--- config/secrets.js
+++ config/secrets.js
@@ -32,5 +32,45 @@ module.exports = {
     clientSecret: process.env.GOOGLE_SECRET || '***',
     callbackURL: '/auth/google/callback',
     passReqToCallback: true
+  },
+
+  instagram: {
+    clientID: process.env.INSTAGRAM_ID || '***',
+    clientSecret: process.env.INSTAGRAM_SECRET || '***',
+    callbackURL: '/auth/instagram/callback',
+    passReqToCallback: true
+  },
+
+  github: {
+    clientID: process.env.GITHUB_ID || '***',
+    clientSecret: process.env.GITHUB_SECRET || '***',
+    callbackURL: '/auth/github/callback',
+    passReqToCallback: true
+  },
+
+  linkedin: {
+    clientID: process.env.LINKEDIN_ID || '***',
+    clientSecret: process.env.LINKEDIN_SECRET || '***',
+    callbackURL: '/auth/linkedin/callback',
+    scope: ['r_fullprofile', 'r_emailaddress', 'r_network'],
+    passReqToCallback: true
+  },
+
+  tumblr: {
+    consumerKey: process.env.TUMBLR_KEY || '***',
+    consumerSecret: process.env.TUMBLR_SECRET || '***',
+    callbackURL: '/auth/tumblr/callback'
+  },
+
+  foursquare: {
+    clientId: process.env.FOURSQUARE_ID || '***',
+    clientSecret: process.env.FOURSQUARE_SECRET || '***',
+    redirectUrl: process.env.FOURSQUARE_REDIRECT_URL || 'http://localhost:3000/auth/foursquare/callback'
+  },
+
+  venmo: {
+    clientId: process.env.VENMO_ID || '1688',
+    clientSecret: process.env.VENMO_SECRET || '***',
+    redirectUrl: process.env.VENMO_REDIRECT_URL || 'http://localhost:3000/auth/venmo/callback'
   }
 };
